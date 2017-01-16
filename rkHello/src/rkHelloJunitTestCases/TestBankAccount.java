7710eeabcfdfe6899d009b0dd724eecd50dee1a8
rk <rajeevkapur@rajeevs-mbp>
2017-01-16 10:41:02 -0600

    updated Testcase for debit method - sufficient funds

--- rkHello/src/rkHelloJunitTestCases/TestBankAccount.java
+++ rkHello/src/rkHelloJunitTestCases/TestBankAccount.java
@@ -1,12 +1,21 @@
 package rkHelloJunitTestCases;
 
+import rkHello.*; 
 import junit.framework.Assert;
-
+import org.junit.Test; 
 
 public class TestBankAccount {
 
 	public TestBankAccount() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@Test
+	public void testDebitWithSufficientFunds() {
+			BankAccount bankAccount = new BankAccount(10); 
+			double amount = bankAccount.debit(5.0); 
+			Assert.assertEquals(5.0, amount);
+		}
 }
+
+
