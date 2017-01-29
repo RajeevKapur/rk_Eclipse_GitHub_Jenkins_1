9db746f95f4a2556a56e56c110b1411eedb3014f
rk <rajeevkapur@rajeevs-mbp>
2017-01-29 11:00:07 -0600

    added project rkHello3

---
+++ rkHello3/src/main/java/rkHello3/HelloWorld.java
@@ -0,0 +1,15 @@
+package rkHello3;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String sayHello(String name){
+		
+		String newName = "Hello World" + name ; 
+		return newName; 
+	}
+	
+}
---
+++ rkHello3/src/test/java/rkHello3/HelloWorldTest.java
@@ -0,0 +1,41 @@
+package rkHello3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHelloWorld() {
+		// fail("Not yet implemented");
+		System.out.println("successfully tested Hello World constructor !");
+	}
+
+	@Test
+	public void testSayHello() {
+		// fail("Not yet implemented");
+		System.out.println("successfully tested testSayHello method !");
+	}
+
+}
